22d2af51a9a53bca9a0a14fe9ebf402ff4b34f17
Paulo Gomes <paulocavalcante001@outlook.com>
2024-01-16 16:45:51 -0300

    renderizando arrays

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -3,7 +3,12 @@ export default class HomeController {
     async index({view}) {
         return view.render('painel.homepage', {
             usuario: 'paulo.v.gomes',
-            sistema: 'caravelas'
+            sistema: 'caravelas',
+            linguagens: [
+                'adonis js',
+                'typescript',
+                'react'
+            ],
         });
     }
 
